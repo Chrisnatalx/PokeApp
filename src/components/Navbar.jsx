6318a62d0c9949195fc81834e0d83bc415183204
Chrisnatalx <natale.christian.a@gmail.com>
2023-01-16 13:47:14 -0300

    Changes in Navbar.jsx, added Logo Pokemon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Logo } from "./Logo";
+import { Flex, Menu, MenuButton } from "@chakra-ui/react";
 
 export const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<>
+			<Flex justifyContent="space-around" bg="black">
+				<Menu>
+					<MenuButton>
+						<Logo />
+					</MenuButton>
+					<MenuButton color="white">Search</MenuButton>
+				</Menu>
+			</Flex>
+		</>
+	);
 };
